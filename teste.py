c14c08611ad4c3c6eaa8a003e5d4a3b8110543fa
Gleilson Pedro Fernandes <gleilson.pedro@saude.local>
2022-04-25 16:26:40 -0300

    adicionado cores

--- teste.py
+++ teste.py
@@ -1,3 +1,12 @@
 import webbrowser
 from tkinter import *
 from tkinter import messagebox
+
+#Cores
+amarelo = '#FFD700'
+laranja = '#FF8C00'
+vermelho = '#FF0000'
+roxo = '#836FFF'
+azul = '#1E90FF'
+verde = '#3CB371'
+cinza = '#C0C0C0'
